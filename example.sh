13eec5ce16698cfce07445e928185bf70b03f824
Wenhong Huang <huangwh79@mail2.sysu.edu.cn>
2025-10-21 12:11:43 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,3 +1,49 @@
+#!/bin/bash
+# ================================================================
+# Script Name: run_adversarial_watermark_removal.sh
+# Description:
+#   This script runs the Adversarial Visible Watermarking experiment
+#   using the SLBR model (DENet, SplitNet, MNet, etc. are also applicable. 
+#        You only need to (or not) add some fields unique to their models. 
+#        You can find them in the .sh files in their source code.). 
+#   This script launches the training or attack process
+#   with customizable parameters such as learning rate, dataset path,
+#   and attack configuration.
+#
+# Usage:
+#   bash example.sh
+#
+# Notes:
+#   - Modify the paths below to match your environment.
+#   - Requires a CUDA-capable GPU and dependencies from requirements.txt.
+#
+# Arguments:
+#   --epochs             Number of training epochs.
+#   --schedule           Learning rate schedule strategy.
+#   --lr                 Initial learning rate.
+#   --resume             Path to the pretrained watermark removal model checkpoint.
+#   --arch               Model architecture to use (e.g., SLBR).
+#   -c                   Path to save.
+#   --attack_method      Adversarial attack method (e.g., pgd_inn, pgd).
+#   --limited-dataset    Whether to use a limited dataset (1 = True).
+#   --use_rie            Enable the RIE module to insert perturabtion.
+#   --epsilon            Perturbation limit for general adversarial attack (e.g., pgd).
+#   --step_alpha         Step size for PGD attack.
+#   --stopnum            How many adversairal images to generate.
+#   --iters              Number of general attack iterations.
+#   --rie_iters          Number of the RIE module iterations.
+#   --simage             Whether to save adversarial images.
+#   --lambda_p           Loss weight for perceptual loss.
+#   --input-size         Input image size.
+#   --train-batch        Training batch size.
+#   --test-batch         Testing batch size.
+#   --use_refine         Enable refinement module, only used for the SLBR model.
+#   --base-dir           Path to the dataset root directory.
+#   --data               Dataset name (e.g., 'multi', 'full', '10kgray', '10kmid', etc.).
+#
+# Example:
+#   bash example.sh
+# ================================================================
 
 set -ex
  CUDA_VISIBLE_DEVICES=0 python adversarial_visible_watermarking/main.py  --epochs 1\
